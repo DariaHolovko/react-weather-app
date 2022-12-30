dccba52aab24dd013bedbb6ce707918e580dcad9
DariaHolovko <holovko2003@gmail.com>
2022-12-30 17:00:10 -0500

    updated weather icons

--- src/weatherIcon.js
+++ src/weatherIcon.js
@@ -1,38 +1,34 @@
 import React from "react";
-import clearSkyDay from "./img/sunny.png";
-import clearSkyNight from "./img/moon.png";
-import fewCloudsNight from "./img/clear-cloudy-1.png";
-import fewcloudsDay from "./img/clear-cloudy.png";
-import cloudy from "./img/cloudy.png";
-import mostlyCloudy from "./img/mostly-cloudy.png";
-import showerRain from "./img/showers.png";
-import rainDay from "./img/drizzle.png";
-import rainNight from "./img/drizzle-1.png";
-import thunderStorm from "./img/thunderstroms.png";
-import snow from "./img/snow.png";
-import mist from "./img/foggy.png";
+import ReactAnimatedWeather from "react-animated-weather";
 
 export default function WeatherIcon(props) {
   console.log(props.code);
   const codeMapping = {
-    "01d": clearSkyDay,
-    "01n": clearSkyNight,
-    "02d": fewcloudsDay,
-    "02n": fewCloudsNight,
-    "03d": cloudy,
-    "03n": cloudy,
-    "04d": mostlyCloudy,
-    "04n": mostlyCloudy,
-    "09d": showerRain,
-    "09n": showerRain,
-    "10d": rainDay,
-    "10n": rainNight,
-    "11d": thunderStorm,
-    "11n": thunderStorm,
-    "13d": snow,
-    "13n": snow,
-    "50d": mist,
-    "50n": mist,
+    "01d": "CLEAR_DAY",
+    "01n": "CLEAR_NIGHT",
+    "02d": "PARTLY_CLOUDY_DAY",
+    "02n": "PARTLY_CLOUDY_NIGHT",
+    "03d": "CLOUDY",
+    "03n": "CLOUDY",
+    "04d": "CLOUDY",
+    "04n": "CLOUDY",
+    "09d": "SLEET",
+    "09n": "SLEET",
+    "10d": "RAIN",
+    "10n": "RAIN",
+    "11d": "WIND",
+    "11n": "WIND",
+    "13d": "SNOW",
+    "13n": "SNOW",
+    "50d": "FOG",
+    "50n": "FOG",
   };
-  return <img src={codeMapping[props.code]} alt="" />;
+  return (
+    <ReactAnimatedWeather
+      icon={codeMapping[props.code]}
+      color="grey"
+      size={160}
+      animate={true}
+    />
+  );
 }
